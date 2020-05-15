13e425573be69464077084b3cbb04197ed26f09a
Aatu <aatu.riuttamaki@gmail.com>
2020-05-15 22:16:13 +0300

    Mobile move messages

--- message.mjs
+++ message.mjs
@@ -1,2 +1,5 @@
 export const MOBILE_SPAWNED = "mobileSpawned";
 export const MOBILE_DESPAWNED = "mobileDespawned";
+export const MOBILE_MOVE = "mobileMove";
+
+export const MOVE_REQUEST = "moveRequest";
