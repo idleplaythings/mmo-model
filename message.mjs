c5a58766d1f550e8ddd156a2bcf92e52a2fbfd01
Aatu <aatu.riuttamaki@gmail.com>
2020-05-18 17:16:23 +0300

    Move failed message

--- message.mjs
+++ message.mjs
@@ -1,5 +1,6 @@
 export const MOBILE_SPAWNED = "mobileSpawned";
 export const MOBILE_DESPAWNED = "mobileDespawned";
 export const MOBILE_MOVE = "mobileMove";
+export const MOBILE_MOVE_FAILED = "mobileMoveFailed";
 
 export const MOVE_REQUEST = "moveRequest";
