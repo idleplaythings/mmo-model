acead3945c6a1feff5f2192f27ea359eda637cbe
Aatu <aatu.riuttamaki@gmail.com>
2020-07-29 17:33:11 +0300

    Rename messages

--- message.mjs
+++ message.mjs
@@ -3,8 +3,8 @@ export const MOBILE_DESPAWNED = "mobileDespawned";
 export const MOBILE_MOVE = "mobileMove";
 export const MOBILE_MOVE_FAILED = "mobileMoveFailed";
 export const MOBILE_MOVE_REQUEST_FAILED = "mobileMoveRequestFailed";
-export const MOBILE_WORK_PROP_REQUEST = "mobileHarvestPropRequest";
-export const MOBILE_WORK_PROP_FAILED = "mobileHarvestPropFailed";
+export const MOBILE_WORK_PROP_FAILED = "mobileWorkPropFailed";
 export const MOBILE_STATE = "mobileState";
 
 export const MOVE_REQUEST = "moveRequest";
+export const WORK_PROP_REQUEST = "workPropRequest";
